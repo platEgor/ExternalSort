7adea7cdfd9b94895f4ada4cf8b9b2d47b1d47bc
platEgor <plat.egor@gmail.com>
2022-04-18 13:48:48 +0400

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.io.File;
+import java.io.RandomAccessFile;
+
 public class Main {
 
+    public static void sortInd(File f, int[] ind){
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        int n = 20;
+        RandomAccessFile d = new RandomAccessFile("1.dat", "rw");
+        int[] ind = new int[n];
+        for(int i = 0; i < ind.length; i++){
+            ind[i]=i;
+            System.out.printf("%4d",i);
+        }
+        for(int i = 0; i < ind.length; i++){
+            d.write(Math.round(65+ Math.random()*25));
+        }
+        d.seek(0);
+        for(var i=0; i<ind.length;i++){
+            System.out.printf("%4d" d.read());
+        }
+        System.out.println();
+        d.close();
     }
 }
